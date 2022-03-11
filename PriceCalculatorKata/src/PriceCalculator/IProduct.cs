6915380d6f41248d8fba975f379935c22f6ce6dc
sabahb3 <sabahbaara4@gmail.com>
2022-03-11 08:32:21 +0200

    Remove unwanted features, which related to  displaying the product's description

--- PriceCalculatorKata/src/PriceCalculator/IProduct.cs
+++ PriceCalculatorKata/src/PriceCalculator/IProduct.cs
@@ -5,11 +5,7 @@ public interface IProduct
     public string Name { get; set; }
     public int UPC { get; set; }
     public double Price { get; set; }
-
-    public string DisplayProductDescription();
-    public string DisplayProductDescription(Enumerations.ProductDescription type);
     public double CalculateTaxValue();
     public double CalculateDiscountValue();
-
     public double CalculatePriceAfterDiscount();
 }
